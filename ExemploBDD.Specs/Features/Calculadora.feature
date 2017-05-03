7e969bfba13f09434a2820393f21dc6e2885f385
Thiago Camargo <thiago.gcamargo@gmail.com>
2017-05-03 13:44:22 -0300

    Adicionando feature Calculator

---
+++ ExemploBDD.Specs/Features/Calculadora.feature
@@ -0,0 +1,54 @@
+﻿Funcionalidade: Calculadora
+
+Esquema do Cenario: Adicição de dois números
+	Dado que eu insiro o número <Number1> na calculadora
+	E eu também insiro o número <Number2> na calculadora
+	Quando eu pressiono adicionar
+	Então o resultado mostrado na tela será <Result>
+
+Exemplos: 
+	| Number1 | Number2 | Result |
+	| 70      | 50      | 120    |
+	| -70     | -50     | -120   |
+	| 10      | -10     | 0	     |
+	| -42     | 13      | -29    |
+
+
+Esquema do Cenario: Subtração de dois números
+	Dado que eu insiro o número <Number1> na calculadora
+	E eu também insiro o número <Number2> na calculadora
+	Quando eu pressiono subtrair
+	Então o resultado mostrado na tela será <Result>
+
+Exemplos: 
+	| Number1 | Number2 | Result |
+	| -70     | -50     | -20    |
+	| 10      | 2       | 8      |
+	| 10      | -10     | 20     |
+	| -42     | 13      | -55    |
+
+Esquema do Cenario: Multiplicação de dois números
+	Dado que eu insiro o número <Number1> na calculadora
+	E eu também insiro o número <Number2> na calculadora
+	Quando eu pressiono multiplicar
+	Então o resultado mostrado na tela será <Result>
+
+Exemplos: 
+	| Number1 | Number2 | Result |
+	| -1      | -50     | 50     |
+	| 0       | 2       | 0      |
+	| 10      | 22      | 220    |
+	| -42     | 13      | -546   |
+
+Esquema do Cenario: Divisão de dois números
+	Dado que eu insiro o número <Number1> na calculadora
+	E eu também insiro o número <Number2> na calculadora
+	Quando eu pressiono dividir
+	Então o resultado mostrado na tela será <Result>
+
+Exemplos: 
+	| Number1 | Number2 | Result |
+	| -70     | 7       | -10    |
+	| 10      | 10      | 1      |
+	| 22      | 1       | 22     |
+	| 49      | 7       | 7      |
---
+++ ExemploBDD.Specs/Steps/CalculadoraStepDefinition.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+using NUnit.Framework;
+
+namespace ExemploBDD.Specs.Steps
+{
+    [Binding]
+    public sealed class CalculadoraStepDefinition
+    {
+        private readonly Calculator _calculator;
+
+        public CalculadoraStepDefinition(Calculator calculator)
+        {
+            _calculator = calculator;
+        }
+
+        // For additional details on SpecFlow step definitions see http://go.specflow.org/doc-stepdef
+
+        [Given(@"que eu insiro o número (.*) na calculadora")]
+        public void DadoQueEuInsiroONumeroNaCalculadora(int firstNumber)
+        {
+            _calculator.FirstNumber = firstNumber;
+        }
+
+        [Given(@"eu também insiro o número (.*) na calculadora")]
+        public void DadoEuTambemInsiroONumeroNaCalculadora(int secondNumber)
+        {
+            _calculator.SecondNumber = secondNumber;
+        }
+               
+        [Then(@"o resultado mostrado na tela será (.*)")]
+        public void EntaoOResultadoMostradoNaTelaSera(int expectedResult)
+        {
+            Assert.AreEqual(expected: expectedResult, actual: _calculator.Result);
+        }
+
+        [When(@"eu pressiono adicionar")]
+        public void QuandoEuPressionoAdicionar()
+        {
+            _calculator.Add();
+        }
+
+        [When(@"eu pressiono subtrair")]
+        public void QuandoEuPressionoSubtrair()
+        {
+            _calculator.Subtract();
+        }
+
+        [When(@"eu pressiono multiplicar")]
+        public void QuandoEuPressionoMultiplicar()
+        {
+            _calculator.Multiply();
+        }
+
+        [When(@"eu pressiono dividir")]
+        public void QuandoEuPressionoDividir()
+        {
+            _calculator.Divide();
+        }
+    }
+}
